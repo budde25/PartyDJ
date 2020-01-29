f30fcd7f4c9370680ea79cbd5ba37e09990b7618
Ethan Budd <budd@Ethans-iMac-Pro.local>
2020-01-28 20:25:33 -0800

    adding platform code for ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,15 +3,134 @@ import Flutter
 import Firebase
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
+
     
-    override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    FirebaseApp.configure()
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let SpotifyClientID = "12e51e7fd567478db5db871585124355"
+    let SpotifyRedirectURL = URL(string: "dev.budde.spotifyqueue://callback")!
+    
+     // keys
+    static private let kAccessTokenKey = "access-token-key"
+       
+    lazy var configuration = SPTConfiguration(
+        clientID: SpotifyClientID,
+        redirectURL: SpotifyRedirectURL
+    )
+    
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.connectionParameters.accessToken = self.accessToken
+        appRemote.delegate = self
+        return appRemote
+    }()
+    
+    var accessToken = UserDefaults.standard.string(forKey: kAccessTokenKey) {
+        didSet {
+            let defaults = UserDefaults.standard
+            defaults.set(accessToken, forKey: AppDelegate.kAccessTokenKey)
+            defaults.synchronize()
+        }
+    }
+    
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        // Connection was successful, you can begin issuing commands
+        self.appRemote.playerAPI?.delegate = self
+        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
+            if let error = error {
+                debugPrint(error.localizedDescription)
+    }
+  })
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        print("disconnected")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        print("failed")
+    }
+    
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        debugPrint("Track name: %@", playerState.track.name)
+    }
+    
+    func connect() {
+        self.appRemote.authorizeAndPlayURI("spotify:track:20I6sIOMTCkB6w7ryavxtO")
+    }
+    
+    override func applicationWillResignActive(_ application: UIApplication) {
+        if self.appRemote.isConnected {
+            self.appRemote.disconnect()
+        }
+    }
+    
+    override func applicationDidBecomeActive(_ application: UIApplication) {
+        if let _ = self.appRemote.connectionParameters.accessToken {
+            self.appRemote.connect()
   }
 }
 
+
+
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+        
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "dev.budde.spotify_queue",
+                                              binaryMessenger: controller.binaryMessenger)
+    channel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        // Note: this method is invoked on the UI thread.
+        
+        guard call.method == "play_song" else {
+            self.appRemote.authorizeAndPlayURI(call.arguments)
+            return
+        }
+        guard call.method == "play" else {
+           
+            return
+        }
+        guard call.method == "pause" else {
+          
+            return
+        }
+        guard call.method == "skip_next" else {
+            
+            return
+        }
+        guard call.method == "skip_previous" else {
+            
+            return
+        }
+        guard call.method == "connect" else {
+            return
+        }
+    })
+        
+    func startSpotify(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let parameters = appRemote.authorizationParameters(from: url);
+
+        if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+            appRemote.connectionParameters.accessToken = access_token
+            self.accessToken = access_token
+        } else if (parameters?[SPTAppRemoteErrorDescriptionKey]) != nil {
+            // Error case
+        }
+
+        return true
+    }
+        
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+    
+    func spotify(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+  return true
+}
+
+}
